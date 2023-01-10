7cc7484ef034048c0db1598883e9bcb745c37dc2
StevenJiao <syjiao@ualberta.ca>
2023-01-09 18:58:23 -0700

    answering question 8

--- Question2.py
+++ Question2.py
@@ -4,3 +4,6 @@ print(requests.__version__)
 
 # Question 7
 print(requests.get("http://google.com").content)
+
+# Question 8
+print(requests.get("https://raw.githubusercontent.com/StevenJiao/CMPUT404_LAB1/master/Question2.py").content)
