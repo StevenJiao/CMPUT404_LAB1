03b5d36aded75c8f65a133a80b27594f64ddbb38
StevenJiao <syjiao@ualberta.ca>
2023-01-09 19:07:38 -0700

    don't be sloppy

--- Question2.py
+++ Question2.py
@@ -1,10 +1,10 @@
 import requests
 
 # Question 2
-print("QUESTION 2: ",requests.__version__, "\n")
+print("QUESTION 2: ", requests.__version__, "\n")
 
 # Question 7
-print("QUESTION 7:", requests.get("http://google.com").content, "\n")
+print("QUESTION 7: ", requests.get("http://google.com").content, "\n")
 
 # Question 8
-print("QUESTION 8:",requests.get("https://raw.githubusercontent.com/StevenJiao/CMPUT404_LAB1/master/Question2.py").content, "\n")
+print("QUESTION 8: ", requests.get("https://raw.githubusercontent.com/StevenJiao/CMPUT404_LAB1/master/Question2.py").content, "\n")
