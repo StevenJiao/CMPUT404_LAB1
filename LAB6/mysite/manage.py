632fac9d860b64aadcf993fae048c2a50276cea0
StevenJiao <syjiao@ualberta.ca>
2023-02-11 20:09:51 -0700

    moving files into mysite

--- LAB6/mysite/manage.py
+++ LAB6/mysite/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django_on_heroku # top of the file
 
 
 def main():
@@ -20,3 +21,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+django_on_heroku.settings(locals()) # bottom of the file    
